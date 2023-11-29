7742d0cac853d474642216812b49e6abd2f0d9ea
XIANGKE GUO <mbp@XIANGKEs-MacBook-Pro.local>
2023-11-29 09:01:19 -0800

    upgrade

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,9 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
 const app = new Hono()
-app.get('/hi', (c) => c.text('Hello Hono!'))
-
 app.get('/', (c) => {
   let url = c.req.query('r')
+  if(!url) return c.text('Hello!')
   // Header may not contain more than a single header, new line detected
   url.replaceAll('\r','').replaceAll('\n','').replaceAll('%0A','');
 
@@ -16,8 +15,10 @@ app.get('/', (c) => {
   let redirectUrl = parsedUrl.href+='?from=redirect'
   return c.redirect(parsedUrl.href)
 })
-app.get('/test', (c) => c.redirect('https://baidu.com?from=cc'))
+
+
 app.get('/cc1', (c) => c.redirect('https://baidu.com?from=cc1'))
 app.get('/cc2', (c) => c.redirect('https://baidu.com?from=cc2'))
+app.get('/cc3', (c) => c.redirect('https://baidu.com?from=cc3'))
 
 serve(app)
