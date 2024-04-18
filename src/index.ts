108df16d51ae5f417387a7c2801a82e710d9d42e
XIANGKE GUO <mbp@XIANGKEs-MacBook-Pro.local>
2024-04-17 22:59:54 -0700

    add dawei

--- src/index.ts
+++ src/index.ts
@@ -22,5 +22,6 @@ app.get('/cc2', (c) => c.redirect('https://baidu.com?from=cc2'))
 app.get('/cc3', (c) => c.redirect('https://baidu.com?from=cc3'))
 app.get('/365', (c) => c.redirect('https://www.jidu365.com?from=cc'))
 app.get('/zm', (c) => c.redirect('https://influx.simai.life/api?redirect=https://navs.savefamily.net?metric=NFC%26keyword=nav'))
+app.get('/dawei', (c) => c.redirect('https://navs.savefamily.net/?vips=10,15,11,16,17'))
 
 serve(app)
